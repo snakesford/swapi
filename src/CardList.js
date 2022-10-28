f4eeab6fe398fb8aec077636fd6e086e4283eb72
EPA <emmett.paul@icloud.com>
2022-10-28 10:33:47 -0700

    cleaning

--- src/CardList.js
+++ src/CardList.js
@@ -9,15 +9,6 @@ const CardList = ({robots}) => {
           <Card
             key={name}
             maxtemp_c={robots[i].maxtemp_c}
-            // name={robots[i].name} 
-            // height={robots[i].height} 
-            // mass={robots[i].mass} 
-            // hair_color={robots[i].hair_color} 
-            // skin_color={robots[i].skin_color} 
-            // eye_color={robots[i].eye_color}
-            // birth_year={robots[i].birth_year} 
-            // gender={robots[i].gender} 
-            // homeworld={robots[i].homeworld}
           />
         )
       })
