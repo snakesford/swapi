21fbbbce97407b56b47152d25c871ac9bdd6261c
EPA <emmett.paul@icloud.com>
2022-11-16 06:51:36 -0800

    setting state

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,25 +12,26 @@ class App extends Component {
       searchField: '45.57592 -122.85168',
       bool: false,
       url0: 'https://weatherapi-com.p.rapidapi.com/current.json?q=',
-      url1: 'https://aerisweather1.p.rapidapi.com/observations/'
+      url1: 'https://aerisweather1.p.rapidapi.com/observations/',
+      apiKey: '3a836c49fdmshf423ee7c43600d9p130177jsn92668590654f',
+      apiHost0: 'weatherapi-com.p.rapidapi.com',
+      apiHost1: 'aerisweather1.p.rapidapi.com'
     }
   }
 
   async fetchApi() {
+    const thing = () => {
+      if(1===1) {
+        this.state.apiHost0 = this.state.apiHost0
+      }
+    }
       const options = {
         method: 'GET',
         headers: {
-          'X-RapidAPI-Key': '3a836c49fdmshf423ee7c43600d9p130177jsn92668590654f',
-          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+          'X-RapidAPI-Key': ''+this.state.apiKey+'',
+          'X-RapidAPI-Host': ''+this.state.apiHost0+''
         }
       };
-    //   const options = {
-    //     method: 'GET',
-    //     headers: {
-    //       'X-RapidAPI-Key': '3a836c49fdmshf423ee7c43600d9p130177jsn92668590654f',
-    //       'X-RapidAPI-Host': 'aerisweather1.p.rapidapi.com'
-    //     }
-    //   };
       
       fetch(''+this.state.url0+''+this.state.searchField+'', options)
       .then(response => response.json())
@@ -39,9 +40,19 @@ class App extends Component {
     //add check somewhere to make sure string is valid before rerendering
   }
 
+  changeTheField = () => {
+    this.setState({
+      searchField: ''
+    })
+  }
+
   componentDidMount() {
     this.fetchApi()
+    this.changeTheField()
   }
+
+      // this.state.searchField = ''
+
   
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value })
@@ -59,15 +70,11 @@ class App extends Component {
     const { weatherData } = this.state;
     return (
       <div className='tc'>
+          <button className="button button1">Use weatherapi-com</button>
+          <button onClick={this.onButtonPress} className="button button2">Use aerisweather</button>
           <SubmitButton buttonPress={this.onButtonPress}/>
           <SearchBox searchChange={this.onSearchChange}/>
         <CardList weatherData={weatherData} bool={this.state.bool}/>
-        <div className='tc pt4'>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
-        </div>
       </div>
     );
   }
